fa6b9ea24239ac6b20bdc3318d8e9348bf19c865
WooramKang <omelisra@naver.com>
2018-08-17 11:50:24 +0900

    update my works

--- AutoEncoders/autoencoder_AEVB.py
+++ AutoEncoders/autoencoder_AEVB.py
@@ -17,23 +17,24 @@ def img_path_to_encoding(image_path, model):
 
 
 def img_to_encoding(image, model):
-    image = cv2.resize(image, (96, 96))
+    image = cv2.resize(image, (384, 384))
     img = image[...,::-1]
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
-    x_train = np.array([img])
-    embedding = model.predict_on_batch(x_train)
-    return embedding
+    output_arr = np.array([img])
+    return output_arr
 
 
-def main():
+def convolution_AE():
 
     database = {}
+    encoding_dim = 96
+    raw_dim = 384
 
     # load all the images of individuals to recognize into the database
     for file in glob.glob("images/*"):
         identity = os.path.splitext(os.path.basename(file))[0]
         identity = str(identity).split('_')[0]
-        database[identity] = img_path_to_encoding(file, FRmodel)
+        database[identity] = img_path_to_encoding(file)
 
     x_train = [float(value)/256 for index, value in enumerate(database)]
 
@@ -46,7 +47,14 @@ def main():
 
     x_test = [float(value)/256 for index, value in enumerate(database_test)]
 
+    autoencoder = Sequential()
+    autoencoder.add(Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=(384, 384)))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(4, (2, 2), activation='relu', padding='same'))
+    autoencoder.summary()
 
 
 if __name__ == "__main__":
-    main()
+    convolution_AE()
