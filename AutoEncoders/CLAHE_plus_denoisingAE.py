1d53c50cf93f8607c10d3a36f05b8da58867b58e
WooramKang <omelisra@naver.com>
2018-08-21 17:46:55 +0900

    update my works with autoencoder

--- AutoEncoders/CLAHE_plus_denoisingAE.py
+++ AutoEncoders/CLAHE_plus_denoisingAE.py
@@ -7,7 +7,7 @@ from keras.layers import Conv2D, Flatten
 from keras.layers import Reshape, Conv2DTranspose
 from keras.models import Model
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
-from keras.datasets import cifar10
+from keras.datasets import cifar10, mnist
 from keras.utils import plot_model
 from keras import backend as K
 import cv2
@@ -29,7 +29,7 @@ if you need those, i'll send you
 def main_color():
     # load the CIFAR10 data
     (x_train, _), (x_test, _) = cifar10.load_data()
-
+    #(x_train, _), (x_test, _) = mnist.load_data()
     """
     for preprocessing,
     RGB to LAB
@@ -39,16 +39,21 @@ def main_color():
     
     LAB to RGB 
     """
-
     x_train_prime = []
     for _img in x_train:
-        x_train_prime.append(hist.preprocessing_hist(_img))
+        #_img = cv2.cvtColor(_img, cv2.COLOR_GRAY2RGB)
+        #_img = cv2.resize(_img, (32, 32))
+        _img = hist.preprocessing_hist(_img)
+        x_train_prime.append(_img)
     x_train = np.array(x_train_prime)
     print(x_train.shape)
 
     x_test_prime = []
     for _img in x_test:
-        x_test_prime.append(hist.preprocessing_hist(_img))
+        #_img = cv2.cvtColor(_img, cv2.COLOR_GRAY2RGB)
+        #_img = cv2.resize(_img, (32, 32))
+        _img = hist.preprocessing_hist(_img)
+        x_test_prime.append(_img)
     x_test = np.array(x_test_prime)
     print(x_test.shape)
     """
@@ -56,7 +61,7 @@ def main_color():
 
     depending on CLAHE parameters,
     
-
+    depending on dataset, you could use resize and colorize as well 
     """
     img_rows = x_train.shape[1]
     img_cols = x_train.shape[2]
@@ -68,12 +73,13 @@ def main_color():
             os.makedirs(save_dir)
 
     imgs = x_test[:100]
-
+    """
     i = 0
     for _img in imgs:
         i = i+1
         Image.fromarray(_img).save('saved_images/{0}_img_raw.png'.format(i))
-
+    #print raw img each image by image
+    """
     imgs = imgs.reshape((10, 10, img_rows, img_cols, channels))
     imgs = np.vstack([np.hstack(i) for i in imgs])
     Image.fromarray(imgs).save('saved_images/sumof_img_raw.png')
@@ -166,11 +172,13 @@ def main_color():
     imgs = x_decoded[:100]
     print(imgs.shape)
     imgs = (imgs * 255).astype(np.uint8)
+    """
     i = 0
     for _img in imgs:
         i = i + 1
         Image.fromarray(_img).save('saved_images/{0}_img_gen.png'.format(i))
-
+    #print generated img each image by image
+    """
     imgs = imgs.reshape((10, 10, img_rows, img_cols, channels))
     imgs = np.vstack([np.hstack(i) for i in imgs])
     Image.fromarray(imgs).save('saved_images/sumof_img_gen.png')
